2e2c3960d0d3d0dbff53cf9f4a625879f6956d31
r1sou <2198372691@qq.com>
2026-01-13 17:37:11 +0800

    v1.0

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include "node/node.h"
+
+int main(int argc, char **argv){
+    rclcpp::init(argc, argv);
+
+    auto node = std::make_shared<PerceptionNode>();
+    
+    node->start();
+
+    rclcpp::WallRate loop_rate(10);
+
+    while(rclcpp::ok()){
+        node->Display();
+        loop_rate.sleep();
+    }
+
+    rclcpp::shutdown();
+    cv::destroyAllWindows();
+    return 0;
+}
