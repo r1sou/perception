716479bb2bf18019716fa8f5d7919560d0806e11
r1sou <2198372691@qq.com>
2026-03-18 16:48:19 +0800

    write ip

--- scripts/client.py
+++ scripts/client.py
@@ -3,6 +3,15 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import netifaces
 import os,json
 import argparse
+import subprocess
+import pexpect
+
+def fingerprint(server_ip):
+    cmd = f"ssh-keygen -R {server_ip}"
+    subprocess.run(cmd, shell=True, executable="/bin/bash", capture_output=True, text=True, timeout=10)
+
+    cmd = f"ssh-keyscan -t ed25519 {server_ip} >> ~/.ssh/known_hosts"
+    subprocess.run(cmd, shell=True, executable="/bin/bash", capture_output=True, text=True, timeout=10)
 
 def get_wired_ip():
     ip_list = []
@@ -41,8 +50,12 @@ def get_server_ip(net):
             ip = future.result()
             if ip:
                 return ip
-
+    
 def auto_update_server_ip(args):
+
+    host_ip = None
+    server_ip = None
+
     for ip in get_wired_ip():
         server_ip = get_server_ip(ip.split('.')[2])
         if server_ip:
@@ -53,6 +66,30 @@ def auto_update_server_ip(args):
             with open(client_config_path, 'w') as f:
                 json.dump(client_config, f, indent=4, sort_keys=False)
             break
+    
+    if not server_ip:
+        return
+
+    with open("/home/sunrise/Desktop/ip.txt", 'w') as f:
+        f.write(host_ip)
+
+    fingerprint(server_ip)
+
+    cmd = f"scp -r /home/sunrise/Desktop/ip.txt swbot@{host_ip}:/home/swbot/Desktop/"
+    child = pexpect.spawn("/bin/bash", ["-c", cmd], timeout=60, encoding='utf-8')
+
+    try:
+        child.expect(r"password:", timeout=10)
+        child.sendline("passswbot")
+        child.expect(pexpect.EOF, timeout=120)
+    except pexpect.exceptions.TIMEOUT:
+        log_print("Timeout, password prompt not appeared or too slow")
+        return False
+    except pexpect.exceptions.EOF:
+        log_print("Process ended")
+        return False
+    finally:
+        child.close()
 
 def parse_args():
     parser = argparse.ArgumentParser()
